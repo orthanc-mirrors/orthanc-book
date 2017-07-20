8084dca7f7c1a479c4eea0b7b694dece08132519
amazy <none@none>
2017-07-20 09:13:22 +0200

    cleanup

--- Sphinx/buildWithDocker.sh
+++ Sphinx/buildWithDocker.sh
@@ -1,2 +1,4 @@
 docker build -t orthanc-book-builder .
 docker run --rm -v $(pwd):/app orthanc-book-builder
+
+docker rmi orthanc-book-builder  # since we rebuild it anyway, no need to keep it -> don't accumulate <none> images
