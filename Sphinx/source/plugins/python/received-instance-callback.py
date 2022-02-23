a8e3c33e12a95bfee1e09a1038e318618850ed25
Sebastien Jodogne <s.jodogne@gmail.com>
2022-02-23 08:38:50 +0100

    fix sample of ReceivedInstanceCallback for Python

--- Sphinx/source/plugins/python/received-instance-callback.py
+++ Sphinx/source/plugins/python/received-instance-callback.py
@@ -3,6 +3,7 @@ from io import BytesIO
 from pydicom import dcmread, dcmwrite
 from pydicom.filebase import DicomFileLike
 
+import orthanc
 
 # from https://pydicom.github.io/pydicom/stable/auto_examples/memory_dataset.html
 def write_dataset_to_bytes(dataset):
@@ -10,24 +11,24 @@ def write_dataset_to_bytes(dataset):
         memory_dataset = DicomFileLike(buffer)
         dcmwrite(memory_dataset, dataset)
         memory_dataset.seek(0)
-
         return memory_dataset.read()
 
-
 def ReceivedInstanceCallback(receivedDicom):
-
     dataset = dcmread(BytesIO(receivedDicom))
 
     if dataset.PatientID.startswith('001-'):
+        orthanc.LogWarning('Discard instance')
         return orthanc.ReceivedInstanceCallbackResult.DISCARD, None
 
-    if dataset.PatientID.startswith('002-'):
+    elif dataset.PatientID.startswith('002-'):
+        orthanc.LogWarning('Store source instance as it is')
         return orthanc.ReceivedInstanceCallbackResult.KEEP_AS_IS, None
 
-    dataset.PatientName = str(dataset.PatientName).upper()
-    dataset.PatientID = '002-' + dataset.PatientID
-    dataset.InstitutionName = "MY INSTITUTION"
-
-    return orthanc.ReceivedInstanceCallbackResult.MODIFIED, write_dataset_to_bytes(dataset)
+    else:
+        orthanc.LogWarning('Modify the source instance')
+        dataset.PatientName = str(dataset.PatientName).upper()
+        dataset.PatientID = '002-' + dataset.PatientID
+        dataset.InstitutionName = "MY INSTITUTION"
+        return orthanc.ReceivedInstanceCallbackResult.MODIFIED, write_dataset_to_bytes(dataset)
 
 orthanc.RegisterReceivedInstanceCallback(ReceivedInstanceCallback)
