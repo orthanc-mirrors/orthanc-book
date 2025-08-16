7c2fec2ff9ff012dd8eb9d03180bc52db0074173
Benoit Crickboom <bc@orthanc.team>
2025-08-16 13:18:17 +0200

    fixed auto routing bug (DW) in Python page

--- Sphinx/source/plugins/python/autorouting-2.py
+++ Sphinx/source/plugins/python/autorouting-2.py
@@ -1,8 +1,10 @@
 import orthanc
+import json
 
 def OnChange(changeType, level, resourceId):
     if changeType == orthanc.ChangeType.STABLE_STUDY:
         print('Stable study: %s' % resourceId)
-        orthanc.RestApiPostAfterPlugins('/dicom-web/servers/sample/store', resourceId)
+        payload = { "Resources" : [resourceId] }
+        orthanc.RestApiPostAfterPlugins('/dicom-web/servers/sample/stow', payload)
 
 orthanc.RegisterOnChangeCallback(OnChange)
